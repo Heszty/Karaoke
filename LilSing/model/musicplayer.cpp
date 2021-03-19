cd0fd643a5bb3267029fd852658ec280c9985799
BumiBumler <naray.balazs@gmail.com>
2021-03-19 20:03:17 +0100

    MusicPlayer plays music online

--- LilSing/model/musicplayer.cpp
+++ LilSing/model/musicplayer.cpp
@@ -6,18 +6,18 @@
 MusicPlayer::MusicPlayer()
 {
     _player = new QMediaPlayer(this);
-    volume = 2;
-    _player->setVolume(6);
+    volume = 8;
+    _player->setVolume(volume);
     playMusic();
 }
 
 void MusicPlayer::playMusic()
 {
     QMediaPlaylist *playlist = new QMediaPlaylist();
-    playlist->addMedia(QUrl::fromLocalFile("songs/song1.mp3"));
-    playlist->addMedia(QUrl::fromLocalFile("songs/song2.mp3"));
-    playlist->addMedia(QUrl::fromLocalFile("songs/song3.mp3"));
-    playlist->addMedia(QUrl::fromLocalFile("songs/song4.mp3"));
+    playlist->addMedia(QUrl("https://people.inf.elte.hu/wb9alh/songs/song1.mp3"));
+    playlist->addMedia(QUrl("https://people.inf.elte.hu/wb9alh/songs/song2.mp3"));
+    playlist->addMedia(QUrl("https://people.inf.elte.hu/wb9alh/songs/song3.mp3"));
+    playlist->addMedia(QUrl("https://people.inf.elte.hu/wb9alh/songs/song4.mp3"));
     playlist->shuffle();
     playlist->setPlaybackMode(QMediaPlaylist::Loop);
 
